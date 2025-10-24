2acbc957f97cf9e7a5a34d94bcdab6ef5f8e16d0
Prittstick22 <dev.bhatt67890@gmail.com>
2025-10-24 19:34:43 +0100

    feat: first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+   server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8080",
+ },
+})
